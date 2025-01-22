32788acc6752e2081c5cc1b03230ee04784f37f3
Ruslan Nasibullin <43744446+IamKOALA@users.noreply.github.com>
2025-01-22 15:37:52 +0300

    delted

--- src/main/java/com/mikhaylov/spring/springboot/nexign_lottery_rest/exception_handling/NoSuchWinnerException.java
+++
@@ -1,7 +0,0 @@
-package com.mikhaylov.spring.springboot.nexign_lottery_rest.exception_handling;
-
-public class NoSuchWinnerException extends RuntimeException {
-    public NoSuchWinnerException(String message) {
-        super(message);
-    }
-}
